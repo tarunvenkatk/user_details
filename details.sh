63b131455085f08d8e5b39aec5fa75178ee3e3e2
zeus <zeus@zeus.kali>
2022-11-04 21:01:13 +0530

    Hello

---
+++ details.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Please Enter your Name $name:"
+read name
+
+echo "Welcome $name"
+
+echo "First name: $first_name"
+read first_name
+
+echo "Last name: $last_name"
+read last_name
+
+echo "Phone Number: $num"
+read num
+
+echo "E-mail : $mail"
+read mail
+
+echo "Address: $add"
+read add
+
+echo "-------------------------------------------"
+echo "-------------------------------------------"
+
+echo "Your Details"
+echo "Name: $first_name $last_name"
+echo "Phone number: $num"
+echo "E-mail: $mail"
+echo "Address: $add"
